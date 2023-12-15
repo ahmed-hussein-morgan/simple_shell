f2aebbfba5803d991a0f9beaa91f68256b7ab228
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2023-12-15 11:32:46 +0200

    try to create the prompt

---
+++ concepts_tasks/prompt.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * main - prints "$ ",
+ * 	wait for the user to enter a command, 
+ * 	prints it on the next line.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	size_t n = 0;
+	char *buf = malloc(sizeof(char) * SIZE);
+	int read_chars;
+
+
+	do
+	{
+		/* code */
+		write(1, "$ ", 2);
+		read_chars = getline(&buf, &n, stdin);
+	} while (read_chars != EOF);
+	write(1, buf, read_chars);
+	
+	/*while (getline(&buf, &n, stdin) != -1)
+	{
+		write(1, "$ ", 2);
+	}*/
+
+	free(buf);
+
+	return (0);
+}
