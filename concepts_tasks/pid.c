8f0c651e1856af00584e9162d3747f9dce03cd58
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2023-12-15 06:30:00 +0000

    pid.c -> betty - fixed

--- concepts_tasks/pid.c
+++ concepts_tasks/pid.c
@@ -9,11 +9,11 @@
  */
 int main(void)
 {
-    pid_t my_pid, my_parent_pid;
+	pid_t my_pid, my_parent_pid;
 
-    my_pid = getpid();
-    my_parent_pid = getppid();
-    printf("The process id is: %u\n", my_pid);
-    printf("The parent process id is: %u\n", my_parent_pid);
-    return (0);
+	my_pid = getpid();
+	my_parent_pid = getppid();
+	printf("The process id is: %u\n", my_pid);
+	printf("The parent process id is: %u\n", my_parent_pid);
+	return (0);
 }
