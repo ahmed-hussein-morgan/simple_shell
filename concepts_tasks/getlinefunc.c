33e60faff09c9cc625539d37c7d108b60b223fa8
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2023-12-15 10:42:00 +0200

    getline function

--- concepts_tasks/getlinefunc.c
+++ concepts_tasks/getlinefunc.c
@@ -2,12 +2,21 @@
 
 int main(void)
 {
-	char arr[SIZE];
+	char *line_buf = NULL;
+	size_t n = 0;
 	size_t read_chars;
 
 	printf("what is your name?");
-	read_chars = getline(arr, SIZE, STDIN_FILENO);
-	printf("name is %s size is %ld", arr, SIZE);
-	
+	read_chars = getline(&line_buf, &n, stdin);
+	/*getline(&line_buf, &n, STDIN_FILENO);*/
+	if (read_chars == -1)
+	{
+		perror("read line failed");
+		return (1);
+	}
+	printf("name is %s size is %li", line_buf, n);
+
+	free(line_buf);
+
 	return (0);
 }
